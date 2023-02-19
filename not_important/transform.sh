1ca4dfde7d1d90ab56e7d7b8eae0f28e1f22483c
ChingKai Tseng <83930467+tseng91301@users.noreply.github.com>
2023-02-19 22:49:02 +0800

    2023-02-19

---
+++ not_important/transform.sh
@@ -0,0 +1,7 @@
+echo "# seats_generator" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:tseng91301/seats_generator.git
+git push -u origin main
